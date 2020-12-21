7f8518519edcef7904c4f123a79d6175ff7763f6
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-12-21 17:36:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3000;
+
+const allowlist = ["https://kissbankule.vercel.app/"];
+
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
+app.get("/", cors(corsOptionsDelegate), (req, res) => {
+  res.status(200).set({
+    connection: "keep-alive",
+    "cache-control": "no-cache",
+    "content-type": "application/json",
+  });
+  const data = { message: "hello, world!" };
+  setInterval(() => {
+    data.timesamp = Date.now();
+    res.write(JSON.stringify(data));
+  }, 2000);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
