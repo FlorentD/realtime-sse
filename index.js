5937583e84b79dd45c55ea487a0c95990b5c1c78
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-12-22 16:47:44 +0100

    major update

--- index.js
+++ index.js
@@ -1,7 +1,105 @@
+const EventEmitter = require("events");
 const express = require("express");
+const https = require("https");
 const app = express();
 const cors = require("cors");
 const sse = require("server-sent-events");
+const { request, gql } = require("graphql-request");
+const flow = require("lodash/fp/flow");
+const getOr = require("lodash/fp/getOr");
+const map = require("lodash/fp/map");
+const reduce = require("lodash/fp/reduce");
+
+const eventEmitter = new EventEmitter();
+const INTERVAL_CALLING = 5000;
+
+const amounts = {
+  kisskissbankbank: 0,
+  ulule: 0,
+};
+
+const query = gql`
+  {
+    projectsBySelection(scope: popular, first: 16) {
+      edges {
+        node {
+          id
+          name
+          shortDesc
+          publicUrl
+          fundingPercent
+          collectedAmount {
+            cents
+          }
+          collectedCount
+          owner {
+            username
+            avatarImage {
+              url
+            }
+          }
+          image {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+const getKissKissStat = async () => {
+  try {
+    const result = await request(
+      "https://www.kisskissbankbank.com/graphql",
+      query
+    );
+    const amount = flow(
+      getOr([])("projectsBySelection.edges"),
+      map((project) => ({
+        collectedAmount: getOr("")("node.collectedAmount.cents")(project) / 100,
+      })),
+      reduce((acc, project) => acc + project.collectedAmount, 0)
+    )(result);
+    if (amount !== amounts.kisskissbankbank) {
+      eventEmitter.emit("kisskissbankbank", amount);
+      amounts.kisskissbankbank = amount;
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const getUluleState = () => {
+  https
+    .get(
+      "https://api.ulule.com/v1/search/projects?q=sort:popular+status:currently&lang=fr",
+      (res) => {
+        let projects = "";
+        res.on("data", (chunk) => {
+          projects += chunk;
+        });
+        res.on("end", () => {
+          const amount = flow(
+            getOr([])("projects"),
+            reduce((acc, project) => acc + project.amount_raised, 0)
+          )(JSON.parse(projects));
+          if (amount !== amounts.ulule) {
+            eventEmitter.emit("ulule", amount);
+            amounts.ulule = amount;
+          }
+        });
+      }
+    )
+    .on("error", (error) => {
+      console.error(error.message);
+    });
+};
+
+setInterval(() => {
+  getKissKissStat();
+  getUluleState();
+}, INTERVAL_CALLING);
+
 const port = 3000;
 
 const allowlist = ["https://kissbankule.vercel.app/"];
@@ -19,7 +117,25 @@ const corsOptionsDelegate = function (req, callback) {
 app.use("/", express.static("public"));
 
 app.get("/sse", sse, (req, res) => {
-  res.sse("event: coucou\ndata: im from the server\n\n");
+  eventEmitter.on("kisskissbankbank", (amount) => {
+    res.sse(
+      `event: kisskissbankbank\ndata: ${JSON.stringify({
+        amount,
+        update: Date.now(),
+      })}\n\n`
+    );
+  });
+  app.get("/api", (req, res) => {
+    res.json(amounts);
+  });
+  eventEmitter.on("ulule", (amount) => {
+    res.sse(
+      `event: ulule\ndata: ${JSON.stringify({
+        amount,
+        update: Date.now(),
+      })}\n\n`
+    );
+  });
 });
 
 app.listen(process.env.PORT || port, () => {
